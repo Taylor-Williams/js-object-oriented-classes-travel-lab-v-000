c2d628f05ef6783560387a1303babd820cc07a93
Taylor Williams <taylor@thewilliams.net>
2019-02-05 22:27:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Route{
     return hor + vert;
   }
   estimatedTime(peak = false) {
-    let travelTime = blocksTravelled()
+    let travelTime = self.blocksTravelled()
     peak ? travelTime = 2 * travelTime : travelTime = 3 * travelTime
     return travelTime
   }
