16fb015d7aec3b0a6ff21d87ef2f9581c0cf45cc
Taylor Williams <taylor@thewilliams.net>
2019-02-05 21:57:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,17 @@ class Route{
   }
 
   blocksTravelled() {
-    let hor = this.beginningLocation.horizontal - this.endLocation.horizontal
-    let vert = this.beginningLocation.vertical - this.endLocation.vertical
+    let vert = Math.abs(this.beginningLocation.vertical - this.endLocation.vertical)
+    let startHor, endHor
+    for(let i = 0; i < eastWest.length; i++){
+      if(beginningLocation.horizontal === eastWest[i]){
+        startHor = i
+      }
+      if(endLocation.horizontal === eastWest[i]){
+        endHor = i
+      }
+    }
+    let hor = Math.abs(startHor - endHor)
     return hor + vert
   }
 }
