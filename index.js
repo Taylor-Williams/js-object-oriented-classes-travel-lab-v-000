c8bba43652f1faa47b114d13ce08e0b81bdba137
Taylor Williams <taylor@thewilliams.net>
2019-02-05 21:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver{
   }
 
   yearsExperienceFromBeginningOf(endDate){
-    return new Date(endDate).getFullYear() - this.startDate.getFullYear()
+    return new Date(endDate).getFullYear() - this.startDate.getFullYear() + 1
   }
 }
 class Route{
